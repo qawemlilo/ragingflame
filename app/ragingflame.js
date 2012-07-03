8fc1ccc90948307526d4b11d808b1aeb86fbd1a6
Qawelesizwe <qawemlilo@gmail.com>
2012-07-03 16:06:35 +0200

    Added comments to my major files

--- app/ragingflame.js
+++ app/ragingflame.js
@@ -1,9 +1,12 @@
 ﻿var ragingflame = {};
 
+//Gmail application credentials
 ragingflame.email = 'qawemlilo@gmail.com';
 ragingflame.password = 'qerlkxveorabgzrg';
 
+// Form validations object
 ragingflame.validate = {
+    // email validator
     email: function(emailAddress) {
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
         
@@ -14,17 +17,20 @@ ragingflame.validate = {
     fields: function(obj) {
         var errMsg = '', i = 0, validation = {};
         
+        //if name field is empty or too short
         if (!obj.name || obj.name.length < 3 ) {
             errMsg += 'Name';
             i++;
         }
-  
+        
+        //if email field is empty or email is not valid
         if (!obj.email || !this.email(obj.email)) {
             i++;
             if (errMsg) errMsg += ', Email';
                 else errMsg += 'Email';
         }
-  
+        
+        //if textarea field is empty or too short
         if (!obj.textarea || obj.textarea.length < 5) {
             i++;
             if (errMsg) {
