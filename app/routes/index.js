99efa7ada87e24ac6a7a3c93c0fbab3dfa19d2e3
Qawelesizwe <qawemlilo@gmail.com>
2012-07-03 11:37:08 +0200

    Created page variable for all routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,19 +4,19 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Ragigng Flame Laboratory' })
+  res.render('index', { title: 'Ragigng Flame Laboratory', page: 'index' })
 };
 
 exports.about = function(req, res){
-  res.render('about', { title: 'Ragigng Flame Laboratory - About' })
+  res.render('about', { title: 'Ragigng Flame Laboratory - About', page: 'about' })
 };
 
 exports.projects = function(req, res){
-  res.render('projects', { title: 'Ragigng Flame Laboratory - Projects' })
+  res.render('projects', { title: 'Ragigng Flame Laboratory - Projects', page: 'projects' })
 };
 
 exports.contact = function(req, res){
-  res.render('contact', { title: 'Ragigng Flame Laboratory - Contact' })
+  res.render('contact', { title: 'Ragigng Flame Laboratory - Contact', page: 'contact' })
 };
 
 exports.validate = {
